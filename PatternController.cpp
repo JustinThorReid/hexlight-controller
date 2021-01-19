f6155c80dfe58b0ba1b2026d8f7f1741d06e62ae
Thor <thor@mediaserver>
2021-01-19 16:16:23 -0500

    Add test pattern and some serial prints

--- PatternController.cpp
+++ PatternController.cpp
@@ -6,214 +6,267 @@
 #include "PSnake.h"
 #include "PFlood.h"
 #include "PFloodRainbow.h"
+#include "PTest.h"
 
 #define LED_PER_HEX 12 // leds per hex
-#define LINE_0_PIXEL_COUNT 5
-#define LINE_1_PIXEL_COUNT LED_PER_HEX * 5
+#define LINE_1_PIXEL_COUNT 5
+#define LINE_2_PIXEL_COUNT LED_PER_HEX * 5
 #define LINE_3_PIXEL_COUNT LED_PER_HEX * 4
 #define LINE_4_PIXEL_COUNT LED_PER_HEX * 4
 #define LINE_5_PIXEL_COUNT LED_PER_HEX * 5
 #define LINE_6_PIXEL_COUNT LED_PER_HEX * 5
 
-#define LINE_0_PIN 0
-#define LINE_1_PIN 23
-#define LINE_3_PIN 32
-#define LINE_4_PIN 27
-#define LINE_5_PIN 25
-#define LINE_6_PIN 33
+#define LINE_1_PIN 27
+#define LINE_2_PIN 26
+#define LINE_3_PIN 25
+#define LINE_4_PIN 4
+#define LINE_5_PIN 16
+#define LINE_6_PIN 17
 
-PatternController::PatternController() {
+PatternController::PatternController()
+{
   this->currentPattern = NULL;
+  Serial.println("Create pattern controller");
 
   // Configure FastLED
-  this->leds[0] = new CRGB[LINE_0_PIXEL_COUNT];
-  this->leds[1] = new CRGB[LINE_1_PIXEL_COUNT];
+  this->leds[0] = new CRGB[LINE_1_PIXEL_COUNT];
+  this->leds[1] = new CRGB[LINE_2_PIXEL_COUNT];
   this->leds[2] = new CRGB[LINE_3_PIXEL_COUNT];
   this->leds[3] = new CRGB[LINE_4_PIXEL_COUNT];
   this->leds[4] = new CRGB[LINE_5_PIXEL_COUNT];
-  this->leds[5] = new CRGB[LINE_6_PIXEL_COUNT];   
-  
-  //FastLED.addLeds<WS2812, LINE_0_PIN, GRB>(this->leds[0], LINE_0_PIXEL_COUNT);
-  FastLED.addLeds<WS2812, LINE_1_PIN, GRB>(this->leds[1], LINE_1_PIXEL_COUNT);
+  this->leds[5] = new CRGB[LINE_6_PIXEL_COUNT];
+
+  Serial.println("LEDs allocated");
+
+  FastLED.addLeds<WS2812, LINE_1_PIN, GRB>(this->leds[0], LINE_1_PIXEL_COUNT);
+  FastLED.addLeds<WS2812, LINE_2_PIN, GRB>(this->leds[1], LINE_2_PIXEL_COUNT);
   FastLED.addLeds<WS2812, LINE_3_PIN, GRB>(this->leds[2], LINE_3_PIXEL_COUNT);
   FastLED.addLeds<WS2812, LINE_4_PIN, GRB>(this->leds[3], LINE_4_PIXEL_COUNT);
   FastLED.addLeds<WS2812, LINE_5_PIN, GRB>(this->leds[4], LINE_5_PIXEL_COUNT);
   FastLED.addLeds<WS2812, LINE_6_PIN, GRB>(this->leds[5], LINE_6_PIXEL_COUNT);
+
+  Serial.println("Fast LED setup");
 }
 
-void PatternController::setType(uint8_t patternId) {
+void PatternController::setType(uint8_t patternId)
+{
   this->unset();
-  
+
+  Serial.print("Setting pattern: ");
+  Serial.println(patternId);
+
   // Create new pattern
-  switch (patternId) {
-    case 0:
-      this->currentPattern = new P_Walk(this);
-      break;
-    case 1:
-      this->currentPattern = new P_SolidGlow(this);
-      break;
-    case 2:
-      this->currentPattern = new P_Specks(this);
-      break;
-    case 3:
-      this->currentPattern = new P_Snake(this);
-      break;
-    case 4:
-      this->currentPattern = new P_Flood(this);
-      break;
-    case 5:
-      this->currentPattern = new P_FloodRainbow(this);
-      break;
-    default:
-      this->currentPattern = NULL;
+  switch (patternId)
+  {
+  case 0:
+    this->currentPattern = new P_Walk(this);
+    break;
+  case 1:
+    this->currentPattern = new P_SolidGlow(this);
+    break;
+  case 2:
+    this->currentPattern = new P_Specks(this);
+    break;
+  case 3:
+    this->currentPattern = new P_Snake(this);
+    break;
+  case 4:
+    this->currentPattern = new P_Flood(this);
+    break;
+  case 5:
+    this->currentPattern = new P_FloodRainbow(this);
+    break;
+  case 99:
+    this->currentPattern = new P_Test(this);
+    break;
+  default:
+    Serial.println("Current pattern cleared");
+    this->currentPattern = NULL;
   }
 
-  if(this->currentPattern) this->currentPattern->init();
-  
-  FastLED.show();  
+  if (this->currentPattern)
+    this->currentPattern->init();
+
+  FastLED.show();
 }
 
-void PatternController::setBrightness(uint8_t level) {
+void PatternController::setBrightness(uint8_t level)
+{
   FastLED.setBrightness(level);
 }
 
-void PatternController::tick(unsigned long milli) {
-  if(this->currentPattern) {
+void PatternController::tick(unsigned long milli)
+{
+  if (this->currentPattern)
+  {
     this->currentPattern->tick(milli);
-    FastLED.show();  
+    FastLED.show();
   }
 }
 
-void PatternController::unset() {
-  if(this->currentPattern) delete this->currentPattern;
+void PatternController::unset()
+{
+  if (this->currentPattern)
+    delete this->currentPattern;
+
   this->currentPattern = NULL;
   FastLED.clear(true);
 }
 
-void PatternController::clear() {
+void PatternController::clear()
+{
   FastLED.clear(false);
 }
 
-CRGB PatternController::getHex(uint8_t id) {
-  switch (id) {
-    case 0:
-      return this->leds[0][0];
-      break;
-    case 1 ... 5:
-      return this->leds[1][0];
-      break;
-    case 6 ... 9:
-      return this->leds[2][0];
-      break;
-    case 10 ... 13:
-      return this->leds[3][0];
-      break;
-    case 14 ... 18:
-      return this->leds[4][0];
-      break;
-    case 19 ... 23:
-      return this->leds[5][0];
-      break;
-    default:
-      return this->leds[0][0];
+CRGB PatternController::getHex(uint8_t id)
+{
+  switch (id)
+  {
+  case 0:
+    return this->leds[0][0];
+    break;
+  case 1 ... 5:
+    return this->leds[1][0];
+    break;
+  case 6 ... 9:
+    return this->leds[2][0];
+    break;
+  case 10 ... 13:
+    return this->leds[3][0];
+    break;
+  case 14 ... 18:
+    return this->leds[4][0];
+    break;
+  case 19 ... 23:
+    return this->leds[5][0];
+    break;
+  default:
+    return this->leds[0][0];
   }
 }
 
-void PatternController::setHex(uint8_t id, CRGB color) {
+void PatternController::setHex(uint8_t id, CRGB color)
+{
   CRGB *leds;
   uint16_t pixelStart;
   uint8_t pixelLen;
 
   // Lookup which pixel array to modify and where to start
-  switch (id) {
-    case 0:
-      leds = this->leds[0];
-      pixelLen = 5;
-      pixelStart = 0;
-      break;
-    case 1 ... 5:
-      leds = this->leds[1];
-      pixelLen = LED_PER_HEX;
-      pixelStart = (id - 1) * LED_PER_HEX;
-      break;
-    case 6 ... 9:
-      leds = this->leds[2];
-      pixelLen = LED_PER_HEX;
-      pixelStart = (id - 6) * LED_PER_HEX;
-      break;
-    case 10 ... 13:
-      leds = this->leds[3];
-      pixelLen = LED_PER_HEX;
-      pixelStart = (id - 10) * LED_PER_HEX;
-      break;
-    case 14 ... 18:
-      leds = this->leds[4];
-      pixelLen = LED_PER_HEX;
-      pixelStart = (id - 14) * LED_PER_HEX;
-      break;
-    case 19 ... 23:
-      leds = this->leds[5];
-      pixelLen = LED_PER_HEX;
-      pixelStart = (id - 19) * LED_PER_HEX;
-      break;
-    default:
-      return;
+  switch (id)
+  {
+  case 0:
+    leds = this->leds[0];
+    pixelLen = 5;
+    pixelStart = 0;
+    break;
+  case 1 ... 5:
+    leds = this->leds[1];
+    pixelLen = LED_PER_HEX;
+    pixelStart = (id - 1) * LED_PER_HEX;
+    break;
+  case 6 ... 9:
+    leds = this->leds[2];
+    pixelLen = LED_PER_HEX;
+    pixelStart = (id - 6) * LED_PER_HEX;
+    break;
+  case 10 ... 13:
+    leds = this->leds[3];
+    pixelLen = LED_PER_HEX;
+    pixelStart = (id - 10) * LED_PER_HEX;
+    break;
+  case 14 ... 18:
+    leds = this->leds[4];
+    pixelLen = LED_PER_HEX;
+    pixelStart = (id - 14) * LED_PER_HEX;
+    break;
+  case 19 ... 23:
+    leds = this->leds[5];
+    pixelLen = LED_PER_HEX;
+    pixelStart = (id - 19) * LED_PER_HEX;
+    break;
+  default:
+    return;
   }
-  
-  for(uint8_t i = 0; i < pixelLen; i++) {
+
+  for (uint8_t i = 0; i < pixelLen; i++)
+  {
     leds[pixelStart + i] = color;
   }
 }
 
 const uint8_t PatternController::map[][6] = {{
-  10,6,1,255,19,14 //0
-},{
-  6,2,255,255,255,0 //1
-},{
-  255,255,3,255,1,6 //2
-},{
-  255,4,255,5,255,2 //3
-},{
-  255,255,255,255,3,255 //4
-},{
-  3,255,255,255,255,255 //5
-},{
-  7,255,2,1,0,10 //6
-},{
-  12,8,255,6,10,11 //7
-},{
-  255,255,9,255,7,12 //8
-},{
-  255,255,255,255,255,8 //9
-},{
-  11,7,6,0,14,15 //10
-},{
-  255,12,7,10,15,13 //11
-},{
-  255,255,8,7,11,255 //12
-},{
-  255,255,11,15,16,255 //13
-},{
-  15,10,0,19,21,23 //14
-},{
-  13,11,10,14,23,16 //15
-},{
-  255,13,15,23,17,255 //16
-},{
-  255,16,23,255,18,255 //17
-},{
-  255,17,255,255,255,255 //18
-},{
-  14,0,255,20,255,21 //19
-},{
-  19,255,255,255,255,255 //20
-},{
-  23,14,19,255,22,255 //21
-},{
-  255,21,255,255,255,255 //22
-},{
-  16,15,14,21,255,17 //23
-}};
-const uint8_t* PatternController::getHexNeighboors(uint8_t id) {
+                                                 10, 6, 1, 255, 19, 14 //0
+                                             },
+                                             {
+                                                 6, 2, 255, 255, 255, 0 //1
+                                             },
+                                             {
+                                                 255, 255, 3, 255, 1, 6 //2
+                                             },
+                                             {
+                                                 255, 4, 255, 5, 255, 2 //3
+                                             },
+                                             {
+                                                 255, 255, 255, 255, 3, 255 //4
+                                             },
+                                             {
+                                                 3, 255, 255, 255, 255, 255 //5
+                                             },
+                                             {
+                                                 7, 255, 2, 1, 0, 10 //6
+                                             },
+                                             {
+                                                 12, 8, 255, 6, 10, 11 //7
+                                             },
+                                             {
+                                                 255, 255, 9, 255, 7, 12 //8
+                                             },
+                                             {
+                                                 255, 255, 255, 255, 255, 8 //9
+                                             },
+                                             {
+                                                 11, 7, 6, 0, 14, 15 //10
+                                             },
+                                             {
+                                                 255, 12, 7, 10, 15, 13 //11
+                                             },
+                                             {
+                                                 255, 255, 8, 7, 11, 255 //12
+                                             },
+                                             {
+                                                 255, 255, 11, 15, 16, 255 //13
+                                             },
+                                             {
+                                                 15, 10, 0, 19, 21, 23 //14
+                                             },
+                                             {
+                                                 13, 11, 10, 14, 23, 16 //15
+                                             },
+                                             {
+                                                 255, 13, 15, 23, 17, 255 //16
+                                             },
+                                             {
+                                                 255, 16, 23, 255, 18, 255 //17
+                                             },
+                                             {
+                                                 255, 17, 255, 255, 255, 255 //18
+                                             },
+                                             {
+                                                 14, 0, 255, 20, 255, 21 //19
+                                             },
+                                             {
+                                                 19, 255, 255, 255, 255, 255 //20
+                                             },
+                                             {
+                                                 23, 14, 19, 255, 22, 255 //21
+                                             },
+                                             {
+                                                 255, 21, 255, 255, 255, 255 //22
+                                             },
+                                             {
+                                                 16, 15, 14, 21, 255, 17 //23
+                                             }};
+const uint8_t *PatternController::getHexNeighboors(uint8_t id)
+{
   return map[id];
 }
--- light-controller.ino
+++ light-controller.ino
@@ -1,47 +1,62 @@
 #include "PatternController.h"
 
-#define PRESISTOR_PIN 26 //Photo resistor pin
+#define PRESISTOR_PIN 13 //Photo resistor pin
 #define RANDOM_PIN 36
 #define BUTTON_PIN 14
 
 #define BRIGHTNESS_HIGH 200
 #define BRIGHTNESS_MID 100
 #define BRIGHTNESS_LOW 25
-#define BRIGHT 3800 //4095
-#define DARK 3175 //3100
-#define BOUNDS 100
+#define BRIGHT 4000 //4095
+#define DARK 3175   //3100
+#define BOUNDS 200
 
-volatile int f_timer=0;
+volatile int f_timer = 0;
 PatternController *ledController;
 
-void setup() {
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Startup begin");
+
   // Initialize sensor pins
   pinMode(PRESISTOR_PIN, INPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+  pinMode(BUTTON_PIN, INPUT);
 
   randomSeed(analogRead(RANDOM_PIN));
   ledController = new PatternController();
 
+  Serial.println("Startup start random");
   startRandomPattern();
+
+  Serial.println("Startup complete");
 }
 
 unsigned long startMillis = 0;
 
 bool buttonState = false;
-bool checkButton() {
+bool checkButton()
+{
   bool currentButton = (digitalRead(BUTTON_PIN) == HIGH);
 
   // If the button state is different than last time and it is current pressed
-  bool result = currentButton != buttonState && currentButton;
+  bool result = (currentButton != buttonState) && currentButton;
   // Update button state for next time
   buttonState = currentButton;
+
+  if (result)
+    Serial.println("Button PRESS");
 
   return result;
 }
 
-void loop() {  
+void loop()
+{
+  Serial.println(millis());
+
   // Check button
-  if(checkButton()) {
+  if (checkButton())
+  {
     startRandomPattern();
   }
 
@@ -49,17 +64,23 @@ void loop() {
   ledController->tick(millis() - startMillis);
 }
 
-void startRandomPattern() {
+void startRandomPattern()
+{
   startMillis = millis();
   ledController->setType(random(1, PATTERN_COUNT));
+  //ledController->setType(99);
 }
 
 uint8_t lightLevel = 1;
-uint8_t getLightLevel() {
+uint8_t getLightLevel()
+{
   int light = analogRead(PRESISTOR_PIN);
-  if(light < BRIGHT - BOUNDS && light > DARK + BOUNDS) lightLevel = BRIGHTNESS_MID;
-  if(light > BRIGHT) lightLevel = BRIGHTNESS_HIGH;
-  if(light < DARK) lightLevel = BRIGHTNESS_LOW;
+  if (light < BRIGHT - BOUNDS && light > DARK + BOUNDS)
+    lightLevel = BRIGHTNESS_MID;
+  if (light > BRIGHT)
+    lightLevel = BRIGHTNESS_HIGH;
+  if (light < DARK)
+    lightLevel = BRIGHTNESS_LOW;
 
   return lightLevel;
 }

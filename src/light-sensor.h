f263fde8f88b06c4b6b837b19752bdc2d5974c04
Justin Thor Reid <JustinThorReid@users.noreply.github.com>
2021-04-16 16:39:06 -0400

    Adjust brightness levels and stop listening to mqtt

--- src/light-sensor.h
+++ src/light-sensor.h
@@ -5,11 +5,11 @@
 #include "../constants.c"
 
 #define BRIGHTNESS_HIGH 200
-#define BRIGHTNESS_MID 100
-#define BRIGHTNESS_LOW 25
+#define BRIGHTNESS_MID 50
+#define BRIGHTNESS_LOW 5
 
 #define BRIGHT 4000 //4095
-#define DARK 3175   //3100
+#define DARK 2275   //3100
 #define BOUNDS 200
 
 uint8_t lightLevel = 1;

98adf7395ecc1d83a97b34270d83d24aa7e44688
Justin Thor Reid <JustinThorReid@users.noreply.github.com>
2023-09-15 14:08:00 -0500

    Make the solid glow a little more active

--- src/patterns/PSolidGlow.cpp
+++ src/patterns/PSolidGlow.cpp
@@ -2,17 +2,17 @@
 
 void P_SolidGlow::tick(unsigned long milli) {
   for(uint8_t i = 0; i < HEX_COUNT; i++) {
-    float rotation1 = (milli + this->hexOffsets[i]*4) / 1000.0;
-    float rotation2 = (milli + this->hexOffsets[i]*4) / 1397.0;
+    float rotation1 = (milli + this->hexOffsets[i]*4*this->hexColors[i]) / 1000.0;
+    float rotation2 = (milli + this->hexOffsets[i]*4*this->hexColors[i]) / 1397.0;
   
     float sVariance = (sin(rotation1) + cos(rotation2) + 2.0) / 4.0;
     float vVariance = (sin(rotation2) + cos(rotation1) + 2.0) / 4.0;
     
-    uint8_t h = ((int)this->hexColors[i]) - (60 * sVariance);
+    uint8_t h = ((int)this->hexColors[i]) - (100 * sVariance);
     uint8_t s = max(0, (int)(this->hexOffsets[i] - 150 * sVariance));
-    uint8_t v = max(0, (int)(this->hexOffsets[i] - 100 * vVariance));
+    uint8_t v = max(0, (int)(200 - 10 * vVariance));
 
-    this->parent->setHex(i, CHSV(h, 250, 200));
+    this->parent->setHex(i, CHSV(h, 250, v));
   }
 }
 
